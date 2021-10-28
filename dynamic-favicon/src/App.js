01f4d6a90cd7d938dbe54e436bd3b99cf9c9c06b
Abhinav Gunishety <a.gunishetty@gmail.com>
2021-10-28 14:12:33 +0100

    update favicon based on the counter value

--- dynamic-favicon/src/App.js
+++ dynamic-favicon/src/App.js
@@ -1,18 +1,38 @@
 import React from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.increment = this.increment.bind(this);
     this.decrement = this.decrement.bind(this);
+    this.updateFavicon = this.updateFavicon.bind(this);
     this.state = {
       counter: 0
 
-    </div>
-  );
+    }
+  }
+
+  updateFavicon() {
+    const favicon = document.querySelector("link[rel~='icon']");
+    if (this.state.counter === 0) {
+      favicon.href = '/default.ico';
+    } else if (this.state.counter === 1) {
+      favicon.href = '/1.ico';
+    } else if (this.state.counter === 2) {
+      favicon.href = '/2.ico';
+    } else if (this.state.counter === 3) {
+      favicon.href = '/3.ico';
+    } else if (this.state.counter === 4) {
+      favicon.href = '/4.ico';
+    } else if (this.state.counter === 5) {
+      favicon.href = '/5.ico';
+    } else if (this.state.counter > 5) {
+      favicon.href = '/5plus.ico';
+    }
+
+  }
+
   increment() {
     this.setState({ counter: Math.min(10, this.state.counter + 1) }, () => {
       this.updateFavicon()
