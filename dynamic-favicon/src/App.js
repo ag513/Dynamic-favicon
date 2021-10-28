ad74f7566b53030a7df7e508a3c76357f5b0ea7e
Abhinav Gunishety <a.gunishetty@gmail.com>
2021-10-28 14:12:13 +0100

    added inc and dec function

--- dynamic-favicon/src/App.js
+++ dynamic-favicon/src/App.js
@@ -4,9 +4,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+  constructor(props) {
+    super(props);
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.state = {
+      counter: 0
 
     </div>
   );
+  increment() {
+    this.setState({ counter: Math.min(10, this.state.counter + 1) }, () => {
+      this.updateFavicon()
+    });
+  }
+  decrement() {
+    this.setState({ counter: Math.max(0, this.state.counter - 1) }, () => {
+      this.updateFavicon()
+    });
+  }
   render() {
     return (
       <div className="App">
